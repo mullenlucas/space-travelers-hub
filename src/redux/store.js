71a4dc6791e07253f4651fbcc3a5cdc36e3ba580
Lucas Comamala <lucascomamala@gmail.com>
2022-10-03 11:11:22 -0600

    Create rockets slice

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import logger from 'redux-logger';
 import thunk from 'redux-thunk';
+import rockets from './rocketsSlice';
 
 const store = configureStore({
-  reducer: {},
+  reducer: {
+    rockets,
+  },
   middleware: [thunk, logger],
 });
 
