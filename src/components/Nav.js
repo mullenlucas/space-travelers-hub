982131920bc7ee6b7d7ad4b3b086959f6421bd90
Lucas Comamala <lucascomamala@gmail.com>
2022-10-03 10:15:10 -0600

    Add logo image

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,12 +1,13 @@
 import PropTypes from 'prop-types';
 import { Link, NavLink } from 'react-router-dom';
 import './Nav.css';
+import logo from '../assets/planet.png';
 
 function Nav({ title, routes }) {
   return (
     <>
       <Link className="header-logo" to="/">
-        <img src="logo.png" alt="logo space travelers" />
+        <img src={logo} alt="logo space travelers" className="logo-img" />
         <h1>{title}</h1>
       </Link>
       <nav>
